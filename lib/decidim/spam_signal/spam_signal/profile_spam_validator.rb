8b3cd9edb177976f528e4a471da9a90702eb7e8d
Hadrien Froger <hadrien@octree.ch>
2022-07-26 10:03:04 -0300

    fixup! wip: structure code

--- lib/decidim/spam_signal/spam_signal/profile_spam_validator.rb
+++ lib/decidim/spam_signal/spam_signal/profile_spam_validator.rb
@@ -4,10 +4,15 @@ module Decidim
       extend ActiveSupport::Concern
 
       included do
-        validate :demo
+        validate :demo, on: :update, if: :about_changed?
 
         def demo
-          byebug
+          return if about_valid?(about)
+          errors.add(:about, I18n.t("decidim.spam_signal.errors.spam"))
+          self.update!(blocked_at: DateTime.now)
+        end
+        def about_valid?
+          about.present?
         end
       end
     end
