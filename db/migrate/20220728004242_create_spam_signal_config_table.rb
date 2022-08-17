4a881664ce7dac6c30d7613d7884414a4927d5b3
Hadrien Froger <hadrien@octree.ch>
2022-08-17 10:18:13 -0300

    fix: migration index name

--- db/migrate/20220728004242_create_spam_signal_config_table.rb
+++ db/migrate/20220728004242_create_spam_signal_config_table.rb
@@ -5,7 +5,7 @@ class CreateSpamSignalConfigTable < ActiveRecord::Migration[5.2]
     create_table :spam_signal_config_tables do |t|
       t.integer :decidim_organization_id,
                 foreign_key: true,
-                index: { name: "index_decidim_awesome_on_decidim_organization_id" }
+                index: { name: "index_decidim_spam_signal_on_decidim_organization_id" }
       t.string :profile_scan
       t.string :comment_scan
 
