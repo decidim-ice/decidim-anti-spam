adbfaeb9f9378edd949c89287c3f16441115a556
Renato <renato@octree.ch>
2023-07-26 06:29:07 -0300

    chore: refactoring

--- config/initializers/spam_signal.rb
+++ config/initializers/spam_signal.rb
@@ -3,10 +3,5 @@
 Decidim::SpamSignal::Scans::ScansRepository.instance.register(:forbidden_tlds, ::Decidim::SpamSignal::Scans::ForbiddenTldsScanCommand)
 Decidim::SpamSignal::Scans::ScansRepository.instance.register(:allowed_tlds, ::Decidim::SpamSignal::Scans::AllowedTldsScanCommand)
 Decidim::SpamSignal::Scans::ScansRepository.instance.register(:word, ::Decidim::SpamSignal::Scans::WordScanCommand)
-Decidim::SpamSignal::Scans::ScansRepository.instance.register(
-  :akismet,
-  ::Decidim::SpamSignal::Scans::AkismetScanCommand
-)
-
 Decidim::SpamSignal::Cops::CopsRepository.instance.register(:lock, ::Decidim::SpamSignal::Cops::LockCopCommand)
 Decidim::SpamSignal::Cops::CopsRepository.instance.register(:sinalize, ::Decidim::SpamSignal::Cops::SinalizeCopCommand)
