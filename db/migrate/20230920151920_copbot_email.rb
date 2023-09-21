de3633b9c711ef96d0932c8c79344c73a4b5e1ac
hfroger <hadrien.froger@gmail.com>
2023-09-21 10:44:51 +0100

    fixup: migration (#16)
    fix migrations issues.
    
    if we change the email of the bot email, be sure it is a confirmed email.
    destroy old bot user, and transfert the moderations created to the new cop bot

--- db/migrate/20230920151920_copbot_email.rb
+++ db/migrate/20230920151920_copbot_email.rb
@@ -4,8 +4,11 @@ class CopbotEmail < ActiveRecord::Migration[5.2]
   def change
     # bot email should never be bot@decidim.org
     cop = Decidim::User.where(nickname: "bot").first
-    cop.update(email: ENV.fetch("USER_BOT_EMAIL", "bot@example.org"))
-    Decidim::User.where(email: "bot@decidim.org").each |legacy_bot|
+    cop.email = ENV.fetch("USER_BOT_EMAIL", "bot@example.org")
+    cop.skip_confirmation!
+    cop.save
+    Decidim::User.where(email: "bot@decidim.org").each do |legacy_bot|
+      Decidim::UserReport.where(user: legacy_bot).update(user: cop)
       legacy_bot.destroy
     end
   end
