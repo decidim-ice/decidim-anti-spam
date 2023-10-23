915b05c7d82103aa9117669222980ca323d7b842
hfroger <hadrien.froger@gmail.com>
2023-10-23 16:21:28 +0100

    fix: migration fail if no configuration has been set (#17)

--- db/migrate/20230920151920_copbot_email.rb
+++ db/migrate/20230920151920_copbot_email.rb
@@ -4,9 +4,11 @@ class CopbotEmail < ActiveRecord::Migration[5.2]
   def change
     # bot email should never be bot@decidim.org
     cop = Decidim::User.where(nickname: "bot").first
-    cop.email = ENV.fetch("USER_BOT_EMAIL", "bot@example.org")
-    cop.skip_confirmation!
-    cop.save
+    if cop
+      cop.email = ENV.fetch("USER_BOT_EMAIL", "bot@example.org")
+      cop.skip_confirmation!
+      cop.save
+    end
     Decidim::User.where(email: "bot@decidim.org").each do |legacy_bot|
       Decidim::UserReport.where(user: legacy_bot).update(user: cop)
       legacy_bot.destroy
