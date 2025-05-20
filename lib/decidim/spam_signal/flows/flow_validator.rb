4a0ce86f60ca6a701a6b78baf62aae58f6e76c4f
Hadrien Froger <hadrien@octree.ch>
2025-05-20 14:05:57 +0100

    merge main

--- lib/decidim/spam_signal/flows/flow_validator.rb
+++ lib/decidim/spam_signal/flows/flow_validator.rb
@@ -21,7 +21,6 @@ module Decidim
               active_conditions = run_conditions(flow.conditions, content_for_antispam)
 
               next if active_conditions.empty?
-
               before_antispam
               Decidim::SpamSignal::AntiSpamAction.call(
                 flow,
