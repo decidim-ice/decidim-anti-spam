34cc40d2a2e0b3e7e371b25e46f91dc8b10bcc65
Hadrien Froger <hadrien@octree.ch>
2025-05-20 12:28:57 +0100

    style: rubocop lints

--- app/commands/decidim/spam_signal/actions/forbid_save_action_command.rb
+++ app/commands/decidim/spam_signal/actions/forbid_save_action_command.rb
@@ -6,6 +6,7 @@ module Decidim
       class ForbidSaveActionCommand < ActionCommand
         def call
           return broadcast(:save) unless config["forbid_save_enabled"]
+
           current_locale = suspicious_user.locale || current_organization.default_locale
           error_keys.each do |error_key|
             errors.add(
