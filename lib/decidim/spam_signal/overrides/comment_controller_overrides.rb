c0c6e83ba96e3403ea90bfed6c976b774072a33d
Hadrien Froger <hadrien@octree.ch>
2025-05-21 07:11:07 +0100

    fix: error message on comment edition modal

--- lib/decidim/spam_signal/overrides/comment_controller_overrides.rb
+++ lib/decidim/spam_signal/overrides/comment_controller_overrides.rb
@@ -8,6 +8,21 @@ module Decidim
 
         included do
           alias_method :spam_signal_origin_create, :create
+          alias_method :spam_signal_origin_update, :update
+
+          def update
+            comment = set_comment
+            set_commentable
+            enforce_permission_to(:update, :comment, comment:)
+            @form = Decidim::Comments::CommentForm.from_params(
+              params.merge(commentable: comment.commentable)
+            ).with_context(
+              current_user:,
+              current_organization:
+            )
+            @form.validate
+            spam_signal_origin_update
+          end
 
           def create
             @form = Decidim::Comments::CommentForm.from_params(
