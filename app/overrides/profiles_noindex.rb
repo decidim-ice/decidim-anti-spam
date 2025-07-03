da6abd1792bef820f4e958ea9301ed67d16fcb6f
Renato <renato@octree.ch>
2025-07-03 07:03:19 -0300

    review rubocop and include decidim/searches/index

--- app/overrides/profiles_noindex.rb
+++ app/overrides/profiles_noindex.rb
@@ -5,12 +5,13 @@
 # using Deface gem
 ##
 views = ["decidim/profiles/show",
-         "decidim/user_activities/index"].freeze
+         "decidim/user_activities/index",
+         "decidim/searches/index"].freeze
 
 views.each_with_index do |view, index|
   Deface::Override.new(virtual_path: view,
-                      name: "profiles_noindex_#{index}",
-                      replace_contents: "erb[silent]:contains('content_for :header_snippets do')",
-                      closing_selector: "erb[silent]:contains('end')",  
-                      text: "<meta name=\"robots\" content=\"noindex, nofollow\">")
+                       name: "profiles_noindex_#{index}",
+                       replace_contents: "erb[silent]:contains('content_for :header_snippets do')",
+                       closing_selector: "erb[silent]:contains('end')",
+                       text: "<meta name=\"robots\" content=\"noindex, nofollow\">")
 end
