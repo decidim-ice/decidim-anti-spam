b5cee11ff6a6e06f2a2b2e42e8bc80a30a31b039
Renato <renato@octree.ch>
2025-07-03 06:56:40 -0300

    fix: update to views from 0.29

--- app/overrides/profiles_noindex.rb
+++ app/overrides/profiles_noindex.rb
@@ -5,13 +5,12 @@
 # using Deface gem
 ##
 views = ["decidim/profiles/show",
-         "decidim/user_activities/index",
-         "decidim/user_timeline/index",
-         "decidim/user_conversation/index"].freeze
+         "decidim/user_activities/index"].freeze
 
-views.each do |view|
+views.each_with_index do |view, index|
   Deface::Override.new(virtual_path: view,
-                       name: "profiles_noindex",
-                       insert_after: "erb[loud]:contains('cell \"decidim/profile\"')",
-                       text: "<% content_for :header_snippets do %><meta name='robots' content='noindex,nofollow'><% end %>")
+                      name: "profiles_noindex_#{index}",
+                      replace_contents: "erb[silent]:contains('content_for :header_snippets do')",
+                      closing_selector: "erb[silent]:contains('end')",  
+                      text: "<meta name=\"robots\" content=\"noindex, nofollow\">")
 end
