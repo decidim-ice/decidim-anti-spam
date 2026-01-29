669ad0f2ee12e0ca7ed01e5e36e280baf347f5e1
Hadrien Froger <hadrien@octree.ch>
2026-01-29 09:42:45 +0000

    release(v1.0.6): full 029 support

--- lib/decidim/spam_signal/version.rb
+++ lib/decidim/spam_signal/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds the decidim-meetings version.
   module SpamSignal
     def self.version
-      "1.0.5" # DO NOT UPDATE MANUALLY
+      "1.0.6" # DO NOT UPDATE MANUALLY
     end
 
     def self.decidim_version
