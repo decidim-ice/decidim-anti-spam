a6f722433969c233305bd1909e4c6447a0630871
Renato <renato@octree.ch>
2026-04-27 12:13:12 -0300

    fix: update comment controller error handling to pass @form to error.js.erb

--- lib/decidim/spam_signal/overrides/comment_controller_overrides.rb
+++ lib/decidim/spam_signal/overrides/comment_controller_overrides.rb
@@ -11,19 +11,36 @@ module Decidim
           alias_method :spam_signal_origin_update, :update
 
           def update
-            comment = set_comment
+            set_comment
             set_commentable
             enforce_permission_to(:update, :comment, comment:)
+
             @form = Decidim::Comments::CommentForm.from_params(
               params.merge(commentable: comment.commentable)
             ).with_context(
               current_user:,
-              current_organization:
+              current_organization:,
+              current_component:
             )
-            @form.validate
+
+            Decidim::Comments::UpdateComment.call(comment, @form) do
+              on(:ok) do
+                respond_to do |format|
+                  format.js { render :update }
+                end
+              end
+
+              on(:invalid) do
+                respond_to do |format|
+                  format.js { render :update_error }
+                end
+              end
+            end
           end
 
           def create
+            enforce_permission_to(:create, :comment, commentable:)
+
             @form = Decidim::Comments::CommentForm.from_params(
               params.merge(commentable:)
             ).with_context(
@@ -31,7 +48,21 @@ module Decidim
               current_component:,
               current_user:
             )
-            @form.validate
+            Decidim::Comments::CreateComment.call(@form) do
+              on(:ok) do |comment|
+                handle_success(comment)
+                respond_to do |format|
+                  format.js { render :create }
+                end
+              end
+
+              on(:invalid) do
+                @error = t("create.error", scope: "decidim.comments.comments")
+                respond_to do |format|
+                  format.js { render :error }
+                end
+              end
+            end
           end
         end
       end
